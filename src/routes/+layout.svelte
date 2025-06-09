e4af9b82396b2f340cfb15fc64b7e199f9e5dade
Fede Esteban <fede.est@gmail.com>
2025-06-09 11:15:14 -0300

    Deps for vercel

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
 	import Header from './Header.svelte';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
+
+	injectSpeedInsights();
+	injectAnalytics();
 </script>
 
 <div class="app">
